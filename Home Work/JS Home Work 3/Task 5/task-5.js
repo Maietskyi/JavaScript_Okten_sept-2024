3808652e2890572126a63b525caf3d91de9fc598
Maietskyi <damaietskyi@gmail.com>
2024-11-08 14:56:58 +0200

    Додав Home Work 3

--- Home Work/JS Home Work 3/Task 5/task-5.js
+++ Home Work/JS Home Work 3/Task 5/task-5.js
@@ -18,3 +18,11 @@
 //
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 //
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write('<ul>');
+for (const orderLiness of listOfItems){
+    document.write(`<li>${orderLiness}</li>`);
+}
+document.write('</ul>');
