802434a01fd00d22e2a674dc6947d88fee4c1191
Maietskyi <damaietskyi@gmail.com>
2024-11-10 15:41:42 +0200

    Додав Home Work 4 - task 4

---
+++ Home Work/JS Home Work 4/Task 4/task-4.js
@@ -0,0 +1,8 @@
+// #SIdMd0hQ
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+function arraySearch (array) {
+    for (const item of array){
+        console.log(item);
+    }
+}
