16d9aebbe3d2bd3a875e8d5ad494f0b2210de4a1
Maietskyi <damaietskyi@gmail.com>
2024-11-10 14:25:01 +0200

    Додав Home Work 4 - task 1

---
+++ Home Work/JS Home Work 4/Task 1/task-1.js
@@ -0,0 +1,20 @@
+// #I2XsG6f
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+// 1 варіант
+function raincoat(a,b){
+      return a * b;
+}
+
+let rectangle1 = raincoat(5,8);
+console.log(rectangle1);
+
+// 2 варіант
+
+function raincoat2(a,b){
+    let result = a * b;
+    console.log(result);
+    return result;
+}
+
+raincoat2(7,9);
