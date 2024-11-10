89e3d01db2407250aca2b21ede5526223876821c
Maietskyi <damaietskyi@gmail.com>
2024-11-11 00:00:48 +0200

    Додав Home Work 3 Додаткові завдання

---
+++ Home Work/JS Home Work 3/Additional/Task 1 Additional/task-1-additional.js
@@ -0,0 +1,15 @@
+// #WpkK0ZH1
+// --створити масив з:
+//     - з 5 числових значень
+// - з 5 стічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+
+let number = [55,98,35,78,12];
+console.log(number);
+
+let string = ['Okten','school','Karpaty','Lviv','Home Work'];
+console.log(string);
+
+let mix = ['Okten','321845',true,658,false];
+console.log(mix);
