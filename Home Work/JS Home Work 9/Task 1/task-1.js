99756c8e686ef83386fc13ef6829e934db3775ea
Maietskyi <damaietskyi@gmail.com>
2024-11-22 00:26:08 +0200

    Додав Home Work 9 task 1

---
+++ Home Work/JS Home Work 9/Task 1/task-1.js
@@ -0,0 +1,19 @@
+// #8Nmt60ZT
+// - створити блок,
+//     - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+// - клонувати його повністю, та додати клон в body.
+
+let div = document.createElement('div')
+
+div.classList.add('wrap')
+div.classList.add('collapse')
+div.classList.add('alpha')
+div.classList.add('beta')
+div.innerText = 'Довільний текст';
+div.style.backgroundColor = 'silver';
+div.style.color = 'green';
+div.style.fontSize = '24px';
+
+document.body.appendChild(div);
