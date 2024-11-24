a1897cef209a9ed2a54fa44f5d423adeb68e89e7
Maietskyi <damaietskyi@gmail.com>
2024-11-24 22:24:34 +0200

    Додав Home Work 10 task update 5

---
+++ Home Work/JS Home Work 10/Task 6/task-6.js
@@ -0,0 +1,16 @@
+
+// #Jg0gPO00
+// створити конвертор ваги з кг в фунти. данні
+// заповнюються через інпут. При введенні даних
+// обрахунок стається миттєво, без натискань
+// додаткових кнопок
+
+
+const enteredKilograms = document.getElementById('enteredKilograms');
+let removingPounds = document.getElementById('removingPounds');
+
+
+enteredKilograms.oninput = function () {
+    let result = +enteredKilograms.value * 2.20462;
+    removingPounds.innerText = `${enteredKilograms.value} кг = ${result} фунт`;
+};
